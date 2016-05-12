ad9b75ff0e03cb64b31ab56c3e94df3d5642eb66
Максим Шульгин < >
2016-05-12 22:07:57 +0500

    spider ne zdorovogo cheloveka

--- game/creatures/monsters/spider.js
+++ game/creatures/monsters/spider.js
@@ -19,21 +19,27 @@ function Spider(game, point, player) {
         base: point
     };
 
-    this.health = this.maxHealth = 5;
+    this.health = this.maxHealth = 3;
+
+    this.updateMoving;
 }
+
+Spider.prototype.updateMoving = function(){
+    if((Math.abs(this.player.x-this.x))>(Math.abs(this.player.y-this.y))){
+    this.physics.moveToXY(this, this.x + 1000*((this.player.x-this.x)/(Math.abs(this.player.x-this.x))), this.y, 200);               
+    }else{
+    this.physics.moveToXY(this, this.x , this.y + 1000*((this.player.y-this.y)/(Math.abs(this.player.y-this.y))), 200);                
+    }
+    this.game.time.events.add(2000, this.updateMoving, this);
+};
+
 
 Spider.prototype = Object.create(BaseMonster.prototype);
 Spider.prototype.constructor = BaseMonster;
 Spider.prototype.Name = "Spider";
 
 Spider.prototype.update = function () {
-    var vector = {
-        x: random(-100, 100),
-        y: random(-100, 100)
-    };
-
-    this.physics.accelerateToXY(this, this.x + vector.x, this.y + vector.y, 1000);
-
+   
 
     if (this.physics.distanceBetween(this.player, this) < attack_distance &&
         this.skill.ready()) {
